8d1e1c447ea9cf697b73de23eaea173a261d3e56
Breno <brenogsbao@gmail.com>
2025-10-09 09:39:38 -0300

    Grafico - Dashboard

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import {useNavigate} from "react-router-dom"
+import {Chart} from 'react-google-charts'
+
+//simulando os dados do banco
+
+export const data=[
+    ["Ano","Vendas","Gastos"],
+    ["2021",1500,500],
+    ["2022",2000,900],
+    ["2023",2800,1000],
+    ["2024",3500,1500]
+  ];
+
+  export const options={
+    isStacked:true,
+    height:300,
+    legend:{ position:"top", maxLines:3},
+    vAxis:{minValue:0}
+};
 
 const Dashboard = () => {
+  const navigate=useNavigate();
+
+//  FUNÇÃO handleLogout
+const handleLogout=()=>{
+  localStorage.removeItem("token");
+  navigate("/")
+}
+
+  
+
+
   return (
     <div>
-      <h1 className='text-center font-bold bg-blue-50'>Seja Bem-Vindo ao Dashboard</h1>
+      <h1>Seja Bem-vindo ao Dashboard</h1>
+      <button onClick={handleLogout} className=" mt-6 w-[100px] h-[60px] bg-blue-950 text-white rounded-2xl">
+        Logout
+      </button>
+
+      <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+
+      />
+
+
+
+
+
 
     </div>
   )
